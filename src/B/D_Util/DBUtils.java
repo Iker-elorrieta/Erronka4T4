cac178b226a1427115b3466857d60387875f4cf5
in1dm3 <in1dm3@HZ352032.elorrieta.int>
2023-05-10 09:40:59 +0200

    s2

--- src/B/D_Util/DBUtils.java
+++ src/B/D_Util/DBUtils.java
@@ -4,7 +4,6 @@ package B.D_Util;
  * Data Base configuration 
  */
 public class DBUtils {
-	
 	public static final String URL = "jdbc:mysql://localhost/reto4grupo4";
 	public static final String DRIVER = "com.mysql.jdbc.Driver";
 	public static final String USER = "root";
